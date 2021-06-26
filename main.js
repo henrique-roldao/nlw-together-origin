2f7a1d0bef09800d3039ce779f83d9a2cb8c2cc8
henrique-roldao <henriqueroldao@rede.ulbra.br>
2021-06-26 14:28:13 -0300

    feat(aula 02 finalizada): aprendi varias funcionalidades para layout com Javascript

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Abre e fecha menu quando clicar no hamburger e X
 const nav = document.querySelector('#header nav');
 const toggle = document.querySelectorAll('nav .toggle');
 
@@ -6,3 +7,26 @@ for(const element of toggle) {
     nav.classList.toggle('show')
   })
 }
+
+// Quando clicar em um item, esconder menu
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show')
+  })
+}
+
+// Mudar header da pagina quando der scroll
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', function() {
+  if(window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
