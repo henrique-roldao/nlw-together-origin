a200be190814eb50b4853f28b9aa19933fc30ce6
henrique-roldao <henriqueroldao@rede.ulbra.br>
2021-06-28 01:46:52 -0300

    feat(aula 04 finalizada!): terminando layout da pagina, adicionando botão para subir pagina
    Aprendi a melhorar meu codigo JS reduzindo funcoes.

--- main.js
+++ main.js
@@ -1,59 +1,78 @@
 // Abre e fecha menu quando clicar no hamburger e X
-const nav = document.querySelector('#header nav');
-const toggle = document.querySelectorAll('nav .toggle');
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
-for(const element of toggle) {
-  element.addEventListener('click', function() {
-    nav.classList.toggle('show')
-  })
+for (const element of toggle) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
 }
 
 // Quando clicar em um item, esconder menu
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll("nav ul li a");
 
-for(const link of links) {
-  link.addEventListener('click', function() {
-    nav.classList.remove('show')
-  })
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
 
 // Mudar header da pagina quando der scroll
-const header = document.querySelector('#header');
-const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function() {
-  if(window.scrollY >= navHeight) {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;
+
+  if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
-    header.classList.add('scroll')
+    header.classList.add("scroll");
   } else {
     // menor que a altura do header
-    header.classList.remove('scroll')
+    header.classList.remove("scroll");
   }
-})
+}
 
 // Testimonials slider by swiper
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   pagination: {
-    el: '.swiper-pagination'
+    el: ".swiper-pagination",
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
 });
 
 // Scrollreveal -> Mostrar elementos quando der scroll na pagina
 const scrollReveal = ScrollReveal({
-  origin: 'top',
-  distance: '30px',
+  origin: "top",
+  distance: "30px",
   duration: 700,
-  reset: true
-})
+  reset: true,
+});
 
 scrollReveal.reveal(
   `#home .image, #home, .text,
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links 
-  `, 
-  { interval: 100 })
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+);
+
+// Botão voltar para o topo
+function backToTop() {
+  const backToTopButton = document.querySelector(".back-to-top");
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
+});
